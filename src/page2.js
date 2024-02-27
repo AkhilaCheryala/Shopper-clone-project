3477417e83964bce189d00d20b0fc5c5cdc48740
AkhilaCheryala <akhilacheryala2804@gmail.com>
2024-02-27 16:34:26 +0530

    updated iframe

--- src/page2.js
+++ src/page2.js
@@ -1,9 +1,12 @@
 function openIframe(){
     document.getElementById("IframeContainer").style.display = 'block';
     document.getElementById("overlay").style.display = "block";
+    document.body.style.overflow ="hidden"
+   
 }
 function closeIframe(){
     document.getElementById("IframeContainer").style.display = 'none';
     document.getElementById("overlay").style.display = "none";
+    document.body.style.overflow ="auto"
 }
 document.ElementById("open").addEventListener("click", openIframe);
