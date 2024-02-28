dba80abafbe7522cf1c9a90d2395210f5ee1c199
AkhilaCheryala <akhilacheryala2804@gmail.com>
2024-02-28 21:42:21 +0530

    added bootstrap dropdown menu's

--- src/page2.js
+++ src/page2.js
@@ -10,3 +10,16 @@ function closeIframe(){
     document.body.style.overflow ="auto"
 }
 document.ElementById("open").addEventListener("click", openIframe);
+
+// document.addEventListener("DOMContentLoaded" ,function(){
+//     var catalogItem =document.getElementById('catalog');
+//     var iframeContainer = document.querySelector(".iframe2container");
+//     var iframe = document.querySelector(".hidden-iframe2");
+//     catalogItem.addEventListener("mouseover", function() {
+//         iframe.style.display = "block";
+//     });
+
+//     catalogItem.addEventListener("mouseout", function() {
+//         iframe.style.display = "none";
+//     });
+// });
